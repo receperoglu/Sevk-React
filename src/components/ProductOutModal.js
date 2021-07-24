282aa0d1dbbcb849bddb639a64f04c4fbc8da75e
Recep Eroğlu <receperoglu1@hotmail.com>
2021-07-24 17:36:05 +0300

    mobile view changed

--- src/components/ProductOutModal.js
+++ src/components/ProductOutModal.js
@@ -43,46 +43,51 @@ export default function ProductOutModal({
                       type="text"
                       onChange={(e) => ChangeWayBillId(e.target.value)}
                       placeholder="İrsaliye No"
-                      className="irsaliyeno col-md-1  ms-TextField-field w80"
+                      className="irsaliyeno col-xs-1 paddleft2 ms-TextField-field"
                     />
                     <br />
                   </div>
                   <hr />
-                  <div className="ProductOutList col-md-12">
+                  <div className="ProductOutList padd0 col-xs-12">
                     {OrderList.map((o) => (
                       <div key={o.id}>
-                        <div className="col-md-12 OrderRow">
-                          <div className="col-md-2 fleft">
-                            <input
-                              type="text"
-                              placeholder={o.Piece}
-                              onChange={(e) => ChangePiece(e.target.value)}
-                              className="Piece ms-TextField-field w80"
-                            />
-                          </div>
-                          <div className="col-md-2 fleft">
-                            <input
-                              type="text"
-                              placeholder="Ağırlık"
-                              onChange={(e) => ChangeWeight(e.target.value)}
-                              className="Weight ms-TextField-field w80"
-                            />
-                          </div>
-                          <div className="col-md-2 text-right fleft">
-                            <span className="Transfer TransferBTN ms-Button ms-Button--primary">
-                              <span
-                                onClick={() => SaveProductOut(o.id)}
-                                className="transfersavetext"
-                              >
-                                Kaydet
-                              </span>
-                            </span>
-                          </div>
-                          <div className="col-md-6 fleft">
+                        <div className="col-xs-12 padd0 OrderRow">
+                          <div className="col-xs-6 padd0 padd0 fleft">
                             <span className="minifont">
-                              {o.Dimensions} {o.Color} {o.ProductTypeName}
+                              <b> {o.Piece} AD</b> {o.Dimensions} {o.Color}{" "}
+                              {o.ProductTypeName}
                             </span>
-                          </div>                         
+                          </div>
+                          <div className="col-xs-2 padd0 fleft">
+                            <input
+                              type="text"
+                              placeholder="Adet"
+                              onChange={(e) => ChangePiece(e.target.value)}
+                              className="Piece ms-TextField-field w60"
+                            />
+                          </div>
+                          <div className="col-xs-2 padd0 fleft">
+                            <input
+                              type="text"
+                              placeholder="KG"
+                              onChange={(e) => ChangeWeight(e.target.value)}
+                              className="Weight ms-TextField-field w60"
+                            />
+                          </div>
+                          <div className="col-xs-2 padd0 text-center fleft">
+                            <span
+                              title="Kaydet"
+                              onClick={() => SaveProductOut(o.id)}
+                              className="TransferBTN ms-Button w60 ms-Button--primary"
+                            >
+                              <i
+                                data-icon-name="StatusCircleCheckmark "
+                                className="FabricMDL2Icons"
+                              >
+                                
+                              </i>
+                            </span>
+                          </div>
                         </div>
                         <hr />
                       </div>
