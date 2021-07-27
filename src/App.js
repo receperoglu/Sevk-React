1b2975dd4665a04345f7e5bfc5ba0490e00ebf60
Recep Eroğlu <receperoglu1@hotmail.com>
2021-07-27 22:27:30 +0300

    component removed(App.js)

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React  from "react";
-import MainPage from "./MainPage";
-export default function App() { 
-  return (
-    <div>
-      <MainPage />
-    </div>
-  );
-}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import MainPage from "./MainPage";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<MainPage />, document.getElementById("root"));
