8010ee7d6690546d8bcef07327b43be44e2da6f3
Recep Eroğlu <receperoglu1@hotmail.com>
2021-07-28 12:56:53 +0300

    css improved

--- src/components/Tools/PicturePreview.js
+++ src/components/Tools/PicturePreview.js
@@ -27,58 +27,7 @@ export default function PicturePreview({
         alt="Resim"
       />
       <div className="resources-action-bar">
-        <div className="resources-action-bar__body">
-          <div className="resources-action-bar__side-left">
-            <div className="resources-info-dropdown">
-              <i data-icon-name="Info" className="FabricMDL2Icons">
-                
-              </i>
-              <div className="resources-info-dropdown__text-wrap">
-                <span className="clamped-text">{Articel}</span>
-              </div>
-            </div>
-          </div>
-          <div className="resources-action-bar__side-right">
-            {Btn(
-              "E-posta",
-              `mailto:receperoglu1@hotmail.com?subject=${Articel}&body=$
-      {Path}`,
-              "OutlookLogo"
-            )}
-            {Btn(
-              "Whatsapp",
-              `https://api.whatsapp.com/send?text=${Path}`,
-              "WordLogo"
-            )}{" "}
-            <div className="groupable-buttons">
-              <div className="groupable-buttons__visible-buttons">
-                <span className="hover-dropdown">
-                  <div className="hover-tooltip__tooltip-anchor">
-                    <button
-                      className="button2 button2_view_classic button2_size_n button2_theme_clear-inverse groupable-buttons__visible-button groupable-buttons__visible-button_name_delete "
-                      type="button"
-                    >
-                      <span className="ufo-icon ufo-icon_size_n icon button2__icon button2__icon_side_left">
-                        <i data-icon-name="Delete" className="FabricMDL2Icons">
-                          
-                        </i>
-                      </span>
-                      <span className="button2__text">Sil</span>
-                    </button>
-                  </div>
-                </span>
-                {Rotate(rotate)}
-              </div>
-              <div className="groupable-buttons__more-button-wrap">
-                <span className="hover-dropdown">
-                  <div className="hover-tooltip__tooltip-anchor">
-                    <CancelBtn click={hidePicturePreview} />
-                  </div>
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
+        {ActionBody(Articel, Path, hidePicturePreview, Btn, rotate)}
       </div>
     </div>
   ) : null;
@@ -92,17 +41,66 @@ function Rotate(rotate) {
       className="hover-dropdown"
     >
       <div className="hover-tooltip__tooltip-anchor">
-        <button
-          className="button2  button2_size_n button2_theme_clear-inverse   "
-          type="button"
-        >
-          <span className="ufo-icon ufo-icon_size_n icon button2__icon button2__icon_side_left">
+        <button className="button2   " type="button">
+          <span className="ufo-icon   icon  ">
             <i className="fa fa-repeat"></i>
           </span>
           <span className="button2__text">Döndür</span>
         </button>
       </div>
     </span>
+  );
+}
+function ActionBody(Articel, Path, hidePicturePreview, Btn, rotate) {
+  return (
+    <div className="resources-action-bar__body">
+      <div className="resources-action-bar__side-left">
+        <div className="resources-info-dropdown">
+          <i data-icon-name="Info" className="FabricMDL2Icons">
+            
+          </i>
+          <div className="resources-info-dropdown__text-wrap">
+            <span className="clamped-text">{Articel}</span>
+          </div>
+        </div>
+      </div>
+      <div className="resources-action-bar__side-right">
+        {Btn(
+          "E-posta",
+          `mailto:receperoglu1@hotmail.com?subject=${Articel}&body=${Path}`,
+          "OutlookLogo"
+        )}
+        {Btn(
+          "Whatsapp",
+          `https://api.whatsapp.com/send?text=${Path}`,
+          "WordLogo"
+        )}
+        <div className="groupable-buttons">
+          <div className="groupable-buttons__visible-buttons">
+            <span className="hover-dropdown">
+              <div className="hover-tooltip__tooltip-anchor">
+                <button className="button2 " type="button">
+                  <span className="ufo-icon ufo-icon_size_n icon button2__icon button2__icon_side_left">
+                    <i data-icon-name="Delete" className="FabricMDL2Icons">
+                      
+                    </i>
+                  </span>
+                  <span className="button2__text">Sil</span>
+                </button>
+              </div>
+            </span>
+            {Rotate(rotate)}
+          </div>
+          <div className="groupable-buttons__more-button-wrap">
+            <span className="hover-dropdown">
+              <div className="hover-tooltip__tooltip-anchor">
+                <CancelBtn click={hidePicturePreview} />
+              </div>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 function Btn(text, url, icon) {
@@ -112,10 +110,7 @@ function Btn(text, url, icon) {
         className="BaseDriveContainer fleft  col-xs-4 col-md-4 ShareMail"
         href={url}
       >
-        <div
-          className="button2 button2_view_default button2_tone_transparent button2_size_n button2_theme_normal "
-          type="button"
-        >
+        <div className="button2" type="button">
           <span className="ufo-icon ufo-icon_size_n icon button2__icon button2__icon_side_left">
             <div className="BaseDrive fleft">
               <span className={`DriveIcon ms-svg-Icon ms-Icon--${icon}`}></span>
